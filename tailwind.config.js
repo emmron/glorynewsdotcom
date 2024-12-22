efd68e20d0d5ac0d3bb46280164ce31cce8f71cb
emmett hoolahan <gerhoolahan@icloud.com>
2024-12-22 12:04:55 +0800

    asAS
    Sa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,67 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
-import colors from 'tailwindcss/colors'
-import plugin from 'tailwindcss/plugin'
-
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
       colors: {
-        gray: colors.slate,
-        'purple': {
-          600: '#582C83',
-          700: '#472368',
-          800: '#361A4D',
-          900: '#251232',
+        'glory-purple': {
+          50: '#FAF5FF',
+          100: '#F3E8FF',
+          200: '#E9D5FF',
+          300: '#D8B4FE',
+          400: '#C084FC',
+          500: '#A855F7',
+          600: '#9333EA',
+          700: '#7E22CE',
+          800: '#6B21A8',
+          900: '#581C87'
         }
       },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      typography: (theme) => ({
+      typography: {
         DEFAULT: {
           css: {
             maxWidth: '65ch',
+            color: '#374151',
             a: {
-              color: theme('colors.purple.600'),
-              textDecoration: 'none',
-              fontWeight: '500',
+              color: '#9333EA',
               '&:hover': {
-                color: theme('colors.purple.800'),
-                textDecoration: 'underline',
+                color: '#7E22CE',
               },
-            },
-            h1: {
-              color: theme('colors.gray.900'),
-              fontWeight: '700',
-            },
-            h2: {
-              color: theme('colors.gray.800'),
-              fontWeight: '600',
-            },
-            h3: {
-              color: theme('colors.gray.800'),
-              fontWeight: '600',
-            },
-            'code::before': {
-              content: 'none',
-            },
-            'code::after': {
-              content: 'none',
             },
           },
         },
-      }),
+      },
     },
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    plugin(function({ addBase, theme }) {
-      addBase({
-        'html': { fontFamily: theme('fontFamily.sans') },
-      })
-    }),
   ],
 }
