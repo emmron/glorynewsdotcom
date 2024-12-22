7ce078f331fac413ead974d5249ef568f7bd2f1a
emmett hoolahan <gerhoolahan@icloud.com>
2024-12-22 12:01:39 +0800

    qw
    wqwq

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		global: {}
+	},
+	server: {
+		fs: {
+			allow: ['.']
+		}
+	},
+	optimizeDeps: {
+		exclude: ['@sveltejs/kit']
+	}
 });
